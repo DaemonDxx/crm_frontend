4cb90e3c93898adc3d31612cb5851a32a3c30bf6
DaemonDxx <i79535999238@gmail.com>
2020-09-09 03:53:32 +0700

    add system notifcation in app

--- src/store/index.js
+++ src/store/index.js
@@ -6,12 +6,76 @@ import {Points} from "@/store/points";
 
 Vue.use(Vuex)
 
+const TYPE_MESSAGE = {
+  DONE: 'DONE',
+  ERROR: 'ERROR',
+  WARM: 'WARM'
+}
+
+const ACTION_SHOW_MESSAGE = 'ACTION_SHOW_MESSAGE';
+const ACTION_HIDE_MESSAGE = 'ACTION_HIDE_MESSAGE';
+const ACTION_SEND_REQUEST = 'ACTION_SEND_REQUEST';
+const ACTION_REQUEST_DONE = 'ACTION_REQUEST_DONE';
+const ACTION_REQUEST_ERROR = 'ACTION_REQUEST_ERROR';
+
+const MUTATION_UPDATE_MESSAGE = 'MUTATION_UPDATE_MESSAGE';
+const MUTATION_SET_STATE_MESSAGE = 'MUTATION_SET_STATE_MESSAGE';
+const MUTATION_UPDATE_STATE_REQUEST = 'MUTATION_UPDATE_STATE_REQUEST';
+
 export default new Vuex.Store({
-  state: {
+  state: () => {
+      return {
+        isShowMessage: false,
+        textMessage: '',
+        typeMessage: '',
+        isSendingRequest: false
+      }
   },
   mutations: {
+    [MUTATION_SET_STATE_MESSAGE] (state, isShowMessage) {
+      state.isShowMessage = isShowMessage;
+    },
+    [MUTATION_UPDATE_MESSAGE] (state, {textMessage, typeMessage}) {
+      state.textMessage = textMessage;
+      state.typeMessage = typeMessage;
+    },
+    [MUTATION_UPDATE_STATE_REQUEST] (state, isSendingRequest) {
+      state.isSendingRequest = isSendingRequest;
+    }
+
   },
   actions: {
+    [ACTION_SHOW_MESSAGE] ({commit, dispatch}, message) {
+      commit(MUTATION_UPDATE_MESSAGE, message);
+      commit(MUTATION_SET_STATE_MESSAGE, true);
+      setTimeout(() => dispatch(ACTION_HIDE_MESSAGE), 3000);
+    },
+
+    [ACTION_HIDE_MESSAGE] ({commit}) {
+      commit(MUTATION_SET_STATE_MESSAGE, false);
+      commit(MUTATION_UPDATE_MESSAGE, '');
+    },
+
+    [ACTION_SEND_REQUEST] ({commit}) {
+      commit(MUTATION_UPDATE_STATE_REQUEST, true);
+    },
+
+    [ACTION_REQUEST_DONE] ({commit, dispatch}, textMessage) {
+      commit(MUTATION_UPDATE_STATE_REQUEST, false);
+      dispatch(ACTION_SHOW_MESSAGE, {textMessage, typeMessage: TYPE_MESSAGE.DONE});
+    },
+
+    [ACTION_REQUEST_ERROR] ({commit, dispatch}, textMessage) {
+      commit(MUTATION_UPDATE_STATE_REQUEST, false);
+      dispatch(ACTION_SHOW_MESSAGE, {textMessage, typeMessage: TYPE_MESSAGE.ERROR});
+    }
+
+  },
+  getters: {
+    isShowMessage: state => state.isShowMessage,
+    textMessage: state => state.textMessage,
+    typeMessage: state => state.typeMessage,
+    isSendingRequest: state => state.isSendingRequest,
   },
   modules: {
     Auth,
---
+++ src/store/notification.js
@@ -0,0 +1,19 @@
+
+const ACTION_GET_NUMBER = 'ACTION_GET_NUMBER';
+const ACTION_UPDATE_NOTIFICATION = 'ACTION_UPDATE_NOTIFICATION';
+
+const MUTATION_UPDATE_GET_NUMBER = '';
+const MUTATION_UPDATE_NOTIFICATION = '';
+
+const Notification = {
+    state: () => {
+
+    },
+    actions: {
+
+    },
+    mutations: {},
+    getters: {}
+}
+
+export {Notification}
